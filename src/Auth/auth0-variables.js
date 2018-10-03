90ddf1f4c7d78c522109b5b2849ae3557bd1f908
Russell Ratcliffe <rratcliffe57@gmail.com>
2018-10-02 23:26:41 -0400

    using prod callback

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'deephire2.auth0.com',
   clientId: 'jhzGFZHTv8ehpGskVKxZr_jXOAvKg7DU',
-  callbackUrl: 'http://localhost:8000/user/callback',
-  // callbackUrl: 'https://recruiter.deephire.io/user/callback',
+  // callbackUrl: 'http://localhost:8000/user/callback',
+  callbackUrl: 'https://recruiter.deephire.io/user/callback',
   dbConnectionName: 'EmailDB',
 };
