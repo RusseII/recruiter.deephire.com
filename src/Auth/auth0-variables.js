5ac436642d934d142b00d92db93c28e39a64ba1c
Russell Ratcliffe <rratcliffe57@gmail.com>
2018-10-09 12:28:14 -0400

    Update auth0-variables.js

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,7 @@
 export const AUTH_CONFIG = {
-  domain: 'login.deephire.io',
+//   domain: 'login.deephire.io',
+    domain: 'deephire2.auth0.com',
+
   clientId: 'jhzGFZHTv8ehpGskVKxZr_jXOAvKg7DU',
   // callbackUrl: 'http://localhost:8000/user/callback',
   callbackUrl: 'https://recruiter.deephire.io/user/callback',
