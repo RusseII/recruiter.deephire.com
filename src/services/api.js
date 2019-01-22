ed13fb5d0961e62d4702983672c193f8972cc231
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-01-21 22:58:48 -0500

    updated to prod urls

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,10 @@ import { stringify } from 'qs';
 import request from '@/utils/request';
 
 const hostedURL = 'https://api.deephire.com';
-// const newApi = 'https://a.deephire.com';
+const newApi = 'https://a.deephire.com';
 
 // const hostedURL = 'http://localhost:3001';
-const newApi = 'http://localhost:3000';
+// const newApi = 'http://localhost:3000';
 
 export async function queryProjectNotice() {
   return request('/api/project/notice');
