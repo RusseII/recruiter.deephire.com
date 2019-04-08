5056a097f8dcbbec01898ef40e72ddec7f3df5da
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-04-08 08:12:52 -0400

    turn InfoCardEditable into functional component

--- src/components/InfoCardEditable/index.js
+++ src/components/InfoCardEditable/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Upload, Button, Card, Row, Icon, List, Popconfirm } from 'antd';
 
@@ -15,107 +15,92 @@ const props = {
   },
   defaultFileList: [],
 };
-class InfoCardEditable extends React.Component {
-  state = { modalVisible: false, candidateProfileData: {} };
+const InfoCardEditable = ({ userId, setVideoData, userName, interviewName, email }) => {
+  const [modalVisible, setmodalVisible] = useState(false);
+  const [candidateProfileData, setCandidateProfileData] = useState({});
 
-  componentDidMount() {
-    const { userId } = this.props;
-
+  useEffect(() => {
     getCandidateProfile(userId).then(r => {
-      if (r) this.setState({ candidateProfileData: r });
+      if (r) setCandidateProfileData(r);
     });
-  }
+  }, []);
 
-  toggleModalVisible = () => {
-    const { modalVisible } = this.state;
-    this.setState({
-      modalVisible: !modalVisible,
-    });
+  const toggleModalVisible = () => {
+    setmodalVisible(!modalVisible);
   };
 
-  remove = i => {
-    const { userId } = this.props;
-    const { candidateProfileData } = this.state;
+  const remove = i => {
     candidateProfileData.youtubeLinks.splice(i, 1);
-    this.setState({ candidateProfileData });
-    updateCandidateProfile(userId, candidateProfileData);
+    // setCandidateProfileData(candidateProfileData);
+    updateCandidateProfile(userId, candidateProfileData).then(r => setCandidateProfileData(r));
   };
 
-  addYouTubeLink = link => {
-    const { userId } = this.props;
-
-    const { candidateProfileData } = this.state;
+  const addYouTubeLink = link => {
     const { youtubeLinks } = candidateProfileData;
     if (!youtubeLinks) {
       candidateProfileData.youtubeLinks = [link];
     } else {
       candidateProfileData.youtubeLinks.push(link);
     }
-    this.setState({ candidateProfileData });
-    updateCandidateProfile(userId, candidateProfileData);
+    updateCandidateProfile(userId, candidateProfileData).then(r => setCandidateProfileData(r));
   };
 
-  render() {
-    const { modalVisible, candidateProfileData } = this.state;
+  if (!candidateProfileData) return null;
+  const { youtubeLinks } = candidateProfileData;
 
-    if (!candidateProfileData) return null;
-    const { setVideoData, userName, interviewName, email } = this.props;
-    const { youtubeLinks } = candidateProfileData;
+  return (
+    <Card style={{ marginBottom: '20px' }} hoverable title={userName}>
+      <Row>
+        <Icon type="insurance" /> {interviewName}
+      </Row>
+      <Row>
+        <Icon type="mail" /> {email}
+      </Row>
 
-    return (
-      <Card style={{ marginBottom: '20px' }} hoverable title={userName}>
-        <Row>
-          <Icon type="insurance" /> {interviewName}
-        </Row>
-        <Row>
-          <Icon type="mail" /> {email}
-        </Row>
+      <List
+        locale={{ emptyText: ' ' }}
+        // locale=""
 
-        <List
-          locale={{ emptyText: ' ' }}
-          // locale=""
+        size="small"
+        dataSource={youtubeLinks}
+        renderItem={(item, index) => (
+          <div>
+            <Icon type="youtube" />{' '}
+            <a onClick={() => setVideoData(item, 'YouTube Video')}>
+              {' '}
+              {item.replace('www.youtube.com/watch?v=', 'youtu.be/')}{' '}
+            </a>
+            <Popconfirm
+              placement="rightTop"
+              title="Are you sure you want to delete this?"
+              onConfirm={() => remove(index)}
+              okText="Delete"
+              cancelText="No"
+            >
+              <Icon className={styles.dynamicDeleteButton} type="minus-circle-o" />
+            </Popconfirm>
+          </div>
+        )}
+      />
 
-          size="small"
-          dataSource={youtubeLinks}
-          renderItem={(item, index) => (
-            <div>
-              <Icon type="youtube" />{' '}
-              <a onClick={() => setVideoData(item, 'YouTube Video')}>
-                {' '}
-                {item.replace('www.youtube.com/watch?v=', 'youtu.be/')}{' '}
-              </a>
-              <Popconfirm
-                placement="rightTop"
-                title="Are you sure you want to delete this?"
-                onConfirm={() => this.remove(index)}
-                okText="Delete"
-                cancelText="No"
-              >
-                <Icon className={styles.dynamicDeleteButton} type="minus-circle-o" />
-              </Popconfirm>
-            </div>
-          )}
-        />
-
-        <Row>
-          <Button style={{ marginRight: '20px' }} type="dashed" onClick={this.toggleModalVisible}>
-            <Icon type="plus" /> Add Youtube Link
+      <Row>
+        <Button style={{ marginRight: '20px' }} type="dashed" onClick={toggleModalVisible}>
+          <Icon type="plus" /> Add Youtube Link
+        </Button>
+        <Upload {...props}>
+          <Button>
+            <Icon type="upload" /> Add Document
           </Button>
-          <Upload {...props}>
-            <Button>
-              <Icon type="upload" /> Add Document
-            </Button>
-          </Upload>
-        </Row>
+        </Upload>
+      </Row>
 
-        <AddYTModal
-          visable={modalVisible}
-          toggle={this.toggleModalVisible}
-          addYouTubeLink={this.addYouTubeLink}
-        />
-      </Card>
-    );
-  }
-}
+      <AddYTModal
+        visable={modalVisible}
+        toggle={toggleModalVisible}
+        addYouTubeLink={addYouTubeLink}
+      />
+    </Card>
+  );
+};
 
 export default InfoCardEditable;
