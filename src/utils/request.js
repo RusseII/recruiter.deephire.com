a6cb73263c305ab4b452d067a34d975e58b5dca0
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-04-10 11:44:02 -0400

    ignore 404 errors

--- src/utils/request.js
+++ src/utils/request.js
@@ -151,8 +151,9 @@ export default function request(url, option) {
         router.push('/exception/500');
         return;
       }
-      if (status >= 404 && status < 422) {
-        router.push('/exception/404');
-      }
+    //ignore 404 errors
+//       if (status >= 404 && status < 422) {
+//         router.push('/exception/404');
+//       }
     });
 }
