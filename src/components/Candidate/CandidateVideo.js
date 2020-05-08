6c70c5426268605ce8310d30903f6de361ef623e
russell <russell@deephire.com>
2020-05-08 13:35:33 -0500

    pause with spacebar

--- src/components/Candidate/CandidateVideo.js
+++ src/components/Candidate/CandidateVideo.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import {
   NotificationOutlined,
@@ -8,15 +8,18 @@ import {
 } from '@ant-design/icons';
 import moment from 'moment';
 
-import { Card, Spin, Slider, Space, Button } from 'antd';
+import { Card, Spin, Slider, Space } from 'antd';
 
 import styles from './index.less';
 
+// const marks = { 66: '', 120: '' };
 const formatTime = time => {
-  return moment()
+  const currentTime = moment()
     .startOf('day')
     .seconds(time)
-    .format('H:mm:ss');
+    .format('HH:mm:ss');
+  if (currentTime[0] === '0' && currentTime[1] === '0') return currentTime.slice(3);
+  return currentTime;
 };
 
 const CandidateVideo = ({ currentQuestionText, videoUrl }) => {
@@ -27,6 +30,19 @@ const CandidateVideo = ({ currentQuestionText, videoUrl }) => {
   const [duration, setDuration] = useState(0);
   const [progress, setProgress] = useState({ playedSeconds: 0 });
   const refContainer = useRef(null);
+
+  const handleEnter = event => {
+    const { code } = event;
+    if (code === 'Space') {
+      event.preventDefault();
+      setPlaying(playing => !playing);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleEnter);
+    return () => window.removeEventListener('keydown', handleEnter);
+  }, []);
   return (
     <Spin spinning={!videoUrl}>
       <Card title={currentQuestionText}>
@@ -35,16 +51,6 @@ const CandidateVideo = ({ currentQuestionText, videoUrl }) => {
           onMouseLeave={() => setHover(false)}
           className={styles.playerWrapper}
         >
-          {/* <ReactPlayer
-          youtubeConfig={{ playerVars: { rel: false, modestbranding: true } }}
-          preload
-          controls
-          playing
-          className={styles.reactPlayer}
-          height="100%"
-          width="100%"
-          url={videoUrl}
-        /> */}
           <ReactPlayer
             className={styles.reactPlayer}
             ref={refContainer}
@@ -72,14 +78,15 @@ const CandidateVideo = ({ currentQuestionText, videoUrl }) => {
               width: '100%',
               bottom: 0,
               position: 'absolute',
-              display: hover ? 'block' : 'none',
+              display: hover || !playing ? 'block' : 'none',
               backgroundColor: 'rgba(0,0,0,.7)',
             }}
           >
             <Slider
-              style={{ marginBottom: 0 }}
+              style={{ marginLeft: 8, marginRight: 8, marginBottom: 0 }}
               max={duration}
-              // dots={true}
+              // dots
+              // marks={marks}
               value={progress.playedSeconds}
               onChange={playedSeconds => {
                 setPlaying(false);
@@ -95,6 +102,7 @@ const CandidateVideo = ({ currentQuestionText, videoUrl }) => {
               // step={1}
               tipFormatter={tip => formatTime(tip)}
             />
+
             <Space style={{ marginLeft: 16, marginBottom: 8 }} size="middle">
               {playing ? (
                 <PauseOutlined
@@ -115,13 +123,13 @@ const CandidateVideo = ({ currentQuestionText, videoUrl }) => {
               ) : (
                 <SoundOutlined className={styles.icon} onClick={() => setMuted(muted => !muted)} />
               )}
-              <div style={{ color: 'white' }}>
-                {`${formatTime(progress.playedSeconds)}/${formatTime(duration)}`}
+              <div style={{ fontSize: 12, color: 'white' }}>
+                {`${formatTime(progress.playedSeconds)} / ${formatTime(duration)}`}
               </div>
             </Space>
           </div>
         </div>
-        <Button style={{ marginTop: 24 }}>Create Clips</Button>
+        {/* <Button style={{ marginTop: 24 }}>Create Clips</Button> */}
       </Card>
     </Spin>
   );
