8a4c005672aa164a3324836cdf4a76315ebae060
russell <russell@deephire.com>
2020-07-30 18:50:42 -0500

    enable live interviews for everyone

--- src/pages/LiveInterviews/LiveInterviews.js
+++ src/pages/LiveInterviews/LiveInterviews.js
@@ -1,18 +1,6 @@
 /* eslint-disable camelcase */
 import React, { useState, useContext, useEffect } from 'react';
-import {
-  Card,
-  Typography,
-  Button,
-  Tabs,
-  Spin,
-  Popover,
-  Tooltip,
-  Empty,
-  Tag,
-  Space,
-  ConfigProvider,
-} from 'antd';
+import { Card, Typography, Tabs, Spin, Popover, Tooltip, Tag, Space, ConfigProvider } from 'antd';
 import router from 'umi/router';
 import { ShareAltOutlined, FileAddOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { lowerCaseQueryParams, handleFilter } from '@bit/russeii.deephire.utils.utils';
@@ -256,82 +244,82 @@ const LiveInterviews = () => {
       : null,
   ].filter(item => item !== null);
 
-  const { email } = recruiterProfile || {};
-  if (
-    (email &&
-      (email === 'demo@deephire.com' ||
-        email.includes('deephire.com') ||
-        email.includes('assistinghands') ||
-        email.includes('russell') ||
-        email.includes('apple') ||
-        email.includes('rratcliffe57@gmail.com') ||
-        email.includes('klingcare'))) ||
-    email.includes('clockworkrecruiting.com')
-  ) {
-    const upcomingInterviewsFiltered = liveInterviews.filter(liveInterview => {
-      return new Date(liveInterview.interviewTime[1]) > new Date();
-    });
-    const upcomingInterviews = upcomingInterviewsFiltered.sort((a, b) => {
-      return new Date(a.interviewTime[0]) - new Date(b.interviewTime[0]);
-    });
+  // const { email } = recruiterProfile || {};
+  // if (
+  //   (email &&
+  //     (email === 'demo@deephire.com' ||
+  //       email.includes('deephire.com') ||
+  //       email.includes('assistinghands') ||
+  //       email.includes('russell') ||
+  //       email.includes('apple') ||
+  //       email.includes('rratcliffe57@gmail.com') ||
+  //       email.includes('klingcare'))) ||
+  //   email.includes('clockworkrecruiting.com')
+  // ) {
+  const upcomingInterviewsFiltered = liveInterviews.filter(liveInterview => {
+    return new Date(liveInterview.interviewTime[1]) > new Date();
+  });
+  const upcomingInterviews = upcomingInterviewsFiltered.sort((a, b) => {
+    return new Date(a.interviewTime[0]) - new Date(b.interviewTime[0]);
+  });
 
-    const pastInterviews = liveInterviews.filter(liveInterview => {
-      return new Date(liveInterview.interviewTime[1]) < new Date();
-    });
+  const pastInterviews = liveInterviews.filter(liveInterview => {
+    return new Date(liveInterview.interviewTime[1]) < new Date();
+  });
 
-    return (
-      <>
-        <AntPageHeader
-          title="Live Interviews"
-          subTitle="Schedule and view live two-way interviews"
-          onBack={null}
-          footer={
-            <Tabs
-              onChange={tabKey => {
-                router.push(`/live-interviews/?tab=${tabKey}&page=${page}`);
-                setActiveTab(tabKey);
+  return (
+    <>
+      <AntPageHeader
+        title="Live Interviews"
+        subTitle="Schedule and view live two-way interviews"
+        onBack={null}
+        footer={
+          <Tabs
+            onChange={tabKey => {
+              router.push(`/live-interviews/?tab=${tabKey}&page=${page}`);
+              setActiveTab(tabKey);
+            }}
+            defaultActiveKey={activeTab}
+          >
+            <Tabs.TabPane tab="Upcoming" key="1" />
+            <Tabs.TabPane tab="Completed" key="2" />
+          </Tabs>
+        }
+      />
+      <ConfigProvider
+        renderEmpty={() =>
+          customEmpty(
+            'No Live Interviews Scheduled',
+            null,
+            null,
+            <InviteDrawer execute={execute} />
+          )
+        }
+      >
+        <Card>
+          <TableToolbar
+            extra={<InviteDrawer execute={execute} />}
+            reload={() => setReload(flag => !flag)}
+          />
+          <Spin spinning={false}>
+            {/* <Table columns={columns} dataSource={upcomingInterviews} pagination={false} /> */}
+            <StandardTable
+              onChange={handleChange}
+              selectedRows={null}
+              loading={pending}
+              data={{ list: activeTab === '1' ? upcomingInterviews : pastInterviews }}
+              // size="small"
+              columns={columns}
+              pagination={{
+                defaultCurrent: Number(page),
+                // current: page,
+                onChange: page => {
+                  router.push(`/live-interviews/?tab=${tab}&page=${page}`);
+                },
               }}
-              defaultActiveKey={activeTab}
-            >
-              <Tabs.TabPane tab="Upcoming" key="1" />
-              <Tabs.TabPane tab="Completed" key="2" />
-            </Tabs>
-          }
-        />
-        <ConfigProvider
-          renderEmpty={() =>
-            customEmpty(
-              'No Live Interviews Scheduled',
-              null,
-              null,
-              <InviteDrawer execute={execute} />
-            )
-          }
-        >
-          <Card>
-            <TableToolbar
-              extra={<InviteDrawer execute={execute} />}
-              reload={() => setReload(flag => !flag)}
             />
-            <Spin spinning={false}>
-              {/* <Table columns={columns} dataSource={upcomingInterviews} pagination={false} /> */}
-              <StandardTable
-                onChange={handleChange}
-                selectedRows={null}
-                loading={pending}
-                data={{ list: activeTab === '1' ? upcomingInterviews : pastInterviews }}
-                // size="small"
-                columns={columns}
-                pagination={{
-                  defaultCurrent: Number(page),
-                  // current: page,
-                  onChange: page => {
-                    router.push(`/live-interviews/?tab=${tab}&page=${page}`);
-                  },
-                }}
-              />
-            </Spin>
-            {/* <TabPane tab="Completed Live Interviews" key="2">
+          </Spin>
+          {/* <TabPane tab="Completed Live Interviews" key="2">
                 <Spin spinning={false}>
                   <StandardTable
                     selectedRows={null}
@@ -341,38 +329,35 @@ const LiveInterviews = () => {
                   />
                 </Spin>
               </TabPane> */}
-          </Card>
-        </ConfigProvider>
-      </>
-    );
-  }
-
-  return (
-    // <Title level={4}>
-    //   <div>Coming soon...</div>
-    //   <Button
-    //     size="large"
-    //     type="primary"
-    //     onClick={() => window.open('https://russellratcliffe.typeform.com/to/KkTSNU', '_blank')}
-    //   >
-    //     Join Waitlist
-    //   </Button>
-    // </Title>
-
-    <Empty
-      image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
-      description="Live interviews coming soon..."
-    >
-      <Button
-        onClick={() => {
-          window.open('https://russellratcliffe.typeform.com/to/KkTSNU', '_blank');
-        }}
-        type="primary"
-      >
-        Join Waitlist
-      </Button>
-    </Empty>
+        </Card>
+      </ConfigProvider>
+    </>
   );
+
+  // <Title level={4}>
+  //   <div>Coming soon...</div>
+  //   <Button
+  //     size="large"
+  //     type="primary"
+  //     onClick={() => window.open('https://russellratcliffe.typeform.com/to/KkTSNU', '_blank')}
+  //   >
+  //     Join Waitlist
+  //   </Button>
+  // </Title>
+
+  // <Empty
+  //   image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+  //   description="Live interviews coming soon..."
+  // >
+  //   <Button
+  //     onClick={() => {
+  //       window.open('https://russellratcliffe.typeform.com/to/KkTSNU', '_blank');
+  //     }}
+  //     type="primary"
+  //   >
+  //     Join Waitlist
+  //   </Button>
+  // </Empty>
 };
 
 export default LiveInterviews;
